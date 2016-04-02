c28e86ae8de46521b8974ead0e4fa7684057998d
Abyssion <farad.khoyratee@gmail.com>
2016-04-02 18:35:39 +0200

    Added files via upload

--- FPGA/Matlab/delta_T_generation.m
+++ FPGA/Matlab/delta_T_generation.m
@@ -1,10 +1,24 @@
-n = 64;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Generation of the constant use to calcul th deltaT
+%%  used to know when the system have to send a pulse to
+%%  the transducer
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Creation  :  02/04/2016
+%%  Update    :  02/04/2016
+%%  Created by KHOYRATEE Farad
+%%  Updated by KHOYRATEE Farad
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+n = 64;         %% Shoot number
+
+%%Angle calcul
 for i = 1:n
     phi{1,i} = asin((2*(i-1))/(n));
 end
 
 i = 1;
+
+%% the angle of the motor rotation between two pulse
 while i < (n/2)
     delta_phi{1, i+1} = phi{1,(n/2)+i}- phi{1,(n/2)-i};
     i = i+1;

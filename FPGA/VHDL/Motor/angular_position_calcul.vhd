51671e02262c066b411f33b9f402d568f22c13f8
Abyssion <farad.khoyratee@gmail.com>
2016-04-02 14:14:41 +0200

    Added files via upload

--- FPGA/VHDL/Motor/angular_position_calcul.vhd
+++ FPGA/VHDL/Motor/angular_position_calcul.vhd
@@ -19,9 +19,7 @@ USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 
 -- packages -- 
-use work.constants.all;
 use work.operation.all;
-use work.math_function.all;
 
 -- fixe point --
 use ieee.fixed_float_types.all;
@@ -34,6 +32,7 @@ ENTITY PWM IS
 		
 		--in--
 		speed						: in 	sfixed(E downto -DEC);
+		delta_phi					: in 	sfixed(E downto -DEC);
 		
 		--out--
 		data						: out 	std_logic_vector(31 downto 0);
@@ -42,75 +41,33 @@ ENTITY PWM IS
 	END PWM;
 	
 ARCHITECTURE rtl OF PWM IS
-	signal 	phi_register	:	fixed_point_tab(0 to 1);
-	signal	phi				:	sfixed(E downto -DEC);
-	signal	delta_phi		:	sfixed(E downto -DEC);
+
 	signal 	delta_T			:	sfixed(E downto -DEC);
-	signal 	position		:	integer range 0 to 128;
-	signal	direction		:	std_logic;
+	
 Begin
-
-position_calcul : process(clk)
-	variable counter	:	integer;
-begin
-	if reset = '1' then
-		position	<=	0;
-	elsif rising_edge(clk) then
-		if	to_sfixed(counter, E, -DEC)*DELTA_X = ONE then
-			direction	<= '0';
-		elsif to_sfixed(counter, E, -DEC)*DELTA_X = MINUS_ONE then
-			direction	<= '1';
-		end if;
-		
-		if counter > 127 then
-			counter	<= 0;
-		else
-			counter	<= counter + 1;
-		end if;
-	end if;
-end process;
-
-delta_phi_calcul : process(clk)
-begin
-	if reset='1' then
-		delta_phi		<=	to_sfixed(0, E, -DEC);
-		phi_register	<=	(0 to 1 => to_sfixed(0, E, -DEC));
-	elsif rising_edge(clk) then
-		phi_register(1)		<= 	phi;
-		phi_register(0)		<= 	phi_register(1);
-		delta_phi			<=	phi_register(1) - phi_register(0);
-	end if;
-end process;
 
 delta_T_calcul : process(clk)
 begin
-	if reset='1' then
-		delta_T		<=	to_sfixed(0, E, -DEC);
+	if reset = '1' then
+		delta_T	<=	to_sfixed(0, E, -DEC);
 	elsif rising_edge(clk) then
-		delta_T		<= 	div(delta_phi, speed);
-	end if;
-end process;
-
-phi_calcul : process(clk)
-begin
-	if reset='1' then
-		phi			<=	to_sfixed(0, E, -DEC);
-	elsif rising_edge(clk) then
-		phi			<= 	arcsin(
-							mult(
-								to_sfixed(position, E, -DEC),
-								DELTA_X
-							)
-						);
+		delta_T	<=	div(delta_phi, speed);
 	end if;
 end process;
 
 hv_top : process(clk)
+	variable counter :	integer	:=	0;
 begin
 	if reset = '1' then
 		top_echo	<=	'0';
 	elsif rising_edge(clk) then
-		top_echo	<= '1';
+		if counter > to_integer(delta_T)-1 then
+			counter		<=	'0';
+			top_echo	<= 	'1';
+		else
+			counter		<=	counter + 1;
+			top_echo	<= 	'0';
+		end if;
 	end if;
 end process;
 
